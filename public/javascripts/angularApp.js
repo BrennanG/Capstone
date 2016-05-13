e434cfc3666dddad4aa0a7b306885be4a4f5832d
Brennan <brennangovreau@yahoo.com>
2016-05-13 11:19:54 -0500

    Remove some left over flapper news stuff

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var app = angular.module('biologyGraphingApp', ['ui.router']);
 
 ///////////////////// STATES /////////////////////////////
 app.config(['$stateProvider', '$urlRouterProvider',
