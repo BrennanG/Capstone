4170c1c6002ba35a0f6866f35ad2274abda467fb
Brennan <brennangovreau@yahoo.com>
2016-11-07 17:55:17 -0600

    Add Confirm Password on Registration Page

--- public/javascripts/Controllers/authCtrl.js
+++ public/javascripts/Controllers/authCtrl.js
@@ -2,8 +2,15 @@ angular.module('biologyGraphingApp').controller('AuthCtrl', ['$scope', '$state',
 function($scope, $state, auth) {
 	$scope.user = {};
 	$scope.teacherAccount = false;
+	$scope.error;
+	$scope.user;
 
 	$scope.register = function() {
+		if ($scope.user.password != $scope.user.confirmPassword) {
+			$scope.error = {message: "Passwords must match"};
+			return;
+		}
+
 		if ($scope.teacherAccount) {
 			auth.teacherRegister($scope.user).error(function(error) {
 				$scope.error = error;
