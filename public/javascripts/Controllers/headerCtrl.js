93b6f27b6fdb799e752610488b08d032eb1c0f41
Brennan <brennangovreau@yahoo.com>
2016-11-21 16:43:57 -0600

    Fix smart-table path, add home button

--- public/javascripts/Controllers/headerCtrl.js
+++ public/javascripts/Controllers/headerCtrl.js
@@ -1,9 +1,22 @@
-angular.module('biologyGraphingApp').controller('HeaderCtrl', ['$scope', 'auth',
-function($scope, auth) {
+angular.module('biologyGraphingApp').controller('HeaderCtrl', ['$scope', '$state', 'auth',
+function($scope, $state, auth) {
 	$scope.isLoggedIn = auth.isLoggedIn;
 
   $scope.logout = function() {
-		if ($scope.isLoggedIn) {
+		if ($scope.isLoggedIn()) {
+			auth.logOut();
+		}
+	};
+
+	$scope.goToHome = function() {
+		var accountType = auth.accountType();
+		if (accountType == "student") {
+			$state.go('studentHome');
+		}
+		else if (accountType == "teacher") {
+			$state.go('teacherHome');
+		}
+		else {
 			auth.logOut();
 		}
 	};
